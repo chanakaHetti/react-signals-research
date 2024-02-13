6c14c2b0ce9a9a236dd9dc30168d6b254bef6e67
chanakaHetti <cdileep40@gmail.com>
2024-02-13 23:18:54 +0000

    update the child component

--- src/child-one.js
+++ src/child-one.js
@@ -1,7 +1,5 @@
 const ChildOne = ({ count }) => {
-  console.log('ChildComponent 1 is rendered');
-
-  return <div>{`Count: ${count}`}</div>;
+  return <div>Count: {count}</div>;
 };
 
 export default ChildOne;
