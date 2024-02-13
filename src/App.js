5bfc7a0a2a4d5e1f87daccea84610244f51fb911
chanakaHetti <cdileep40@gmail.com>
2024-02-13 21:37:22 +0000

    Adding the changes for hook usgae

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import ChildOne from './child-one';
+import ChildTwo from './child-two';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={incrementCount}>Increment Count</button>
+      <ChildOne count={count} />
+      <ChildTwo />
     </div>
   );
 }
---
+++ src/child-two.js
@@ -0,0 +1,7 @@
+const ChildTwo = ({ count }) => {
+  console.log('ChildComponent 2 is rendered');
+
+  return <p>Child 2</p>;
+};
+
+export default ChildTwo;
